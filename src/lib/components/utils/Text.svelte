f328ae3cbec7bda3109ebf8d8cb26e3b46748c56
5CYTH3 <ScytheD@outlook.fr>
2022-04-21 22:32:11 +0200

    Added utility component 'Text' (will support dark mode later + allow font-weight management).

---
+++ src/lib/components/utils/Text.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    export let fontWeight: FontWeight;
+
+    type FontWeight =
+        | 'thin'
+        | 'extralight'
+        | 'light'
+        | 'normal'
+        | 'medium'
+        | 'semibold'
+        | 'bold'
+        | 'extrabold'
+        | 'black';
+</script>
+
+<p class="font-prompt font-{fontWeight} text-white">
+    <slot />
+</p>
