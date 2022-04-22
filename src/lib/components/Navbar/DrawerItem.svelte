274a43924f53a1cffbaa11a4c945cb31a0d2ba3c
Nirbose <nirbose2@gmail.com>
2022-04-22 19:29:44 +0200

    add icon for DrawerItem

--- src/lib/components/Navbar/DrawerItem.svelte
+++ src/lib/components/Navbar/DrawerItem.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
+    interface DrawerItemProps {
+        element: string;
+        route: string;
+    }
+
     export let text: string;
-    export let drawerContent: { element: string; route: string }[];
+    export let drawerContent: DrawerItemProps[];
 
     let isOpened: boolean;
 </script>
@@ -8,9 +13,10 @@
 <div>
     <p
         on:click={() => (isOpened = !isOpened)}
-        class="cursor-pointer font-prompt font-normal text-white"
+        class="cursor-pointer font-prompt font-normal text-white flex"
     >
         {text}
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="fill: #fff"><path d="M16.293 9.293 12 13.586 7.707 9.293l-1.414 1.414L12 16.414l5.707-5.707z"></path></svg>
     </p>
     {#if isOpened}
         <div
