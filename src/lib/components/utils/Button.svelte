429928403342d32d52acb8d8e7b3ec916caa6c39
5CYTH3 <ScytheD@outlook.fr>
2022-04-22 09:09:55 +0200

    Added button utility component + all colors from color palette

---
+++ src/lib/components/utils/Button.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    type Colors =
+        | 'original-light-gray'
+        | 'original-purple'
+        | 'original-light-purple'
+        | 'original-gray';
+
+    export let background: Colors;
+    export let onClick: any;
+</script>
+
+<button
+    on:click={onClick}
+    class="bg-{background} text-white shadow-md font-prompt font-bold"
+>
+    <slot />
+</button>
