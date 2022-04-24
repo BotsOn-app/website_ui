2b17532f5f2225a4fa51d750a60db41294b9bb66
5CYTH3 <ScytheD@outlook.fr>
2022-04-24 21:16:05 +0200

    Added Footer to the layout.

---
+++ src/lib/components/Footer/Footer.svelte
@@ -0,0 +1,20 @@
+<script>
+    import Text from '../utils/Text.svelte';
+</script>
+
+<footer class="flex justify-center items-center">
+    <div class="w-4/5 flex items-center h-96">
+        <div class="mr-16">
+            <Text>BotsOn, the customizable bot</Text>
+            <div />
+        </div>
+        <div>
+            <Text>Links :</Text>
+            <ul>
+                <li>General Condition</li>
+                <li>Support Server</li>
+                <li>Other...</li>
+            </ul>
+        </div>
+    </div>
+</footer>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,11 @@
 <script>
     import '../app.css';
     import Navbar from '$lib/components/Navbar/Navbar.svelte';
+    import Footer from '$lib/components/Footer/Footer.svelte';
 </script>
 
 <main class="bg-original-dark">
     <Navbar />
     <slot />
+    <Footer />
 </main>
