e9454948582496a0a584f925036c02f8eff02bc8
5CYTH3 <ScytheD@outlook.fr>
2022-04-24 21:15:28 +0200

    Modified index.svelte and added 2nd and 3rd sections.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -42,7 +42,32 @@
         </div>
     </div>
 </section>
-<section />
+<section
+    class="flex justify-around items-center w-full flex-row-reverse h-[450px]"
+>
+    <div>
+        <Text fontSize="text-4xl" fontWeight="font-bold">
+            Make a bot without bothering you with code
+        </Text>
+        <Text fontSize="text-2xl">
+            We provide the ability to install extensions made by developers who
+            have various objectives.
+        </Text>
+    </div>
+    <img src="" alt="" />
+</section>
+<section class="flex justify-around items-center w-full flex-row h-[450px]">
+    <div>
+        <Text fontSize="text-4xl" fontWeight="font-bold">
+            See your bots stats in live!
+        </Text>
+        <Text fontSize="text-2xl">
+            BotsOn propose a stats page of your bot to know more about what the
+            communities asks for!
+        </Text>
+    </div>
+    <img src="" alt="" />
+</section>
 
 <style>
     .hero-section-title {
