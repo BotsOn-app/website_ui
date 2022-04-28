9d8c744aa381f02c15f170c5166f057ed3b963fd
5CYTH3 <ScytheD@outlook.fr>
2022-04-28 22:22:41 +0200

    Added if statement to check if our array is empty or not.

--- src/routes/extensions.svelte
+++ src/routes/extensions.svelte
@@ -3,6 +3,16 @@
     import Grid from '$lib/components/pages/extensions/Grid.svelte';
     import Text from '$lib/components/utils/Text.svelte';
 
+    const db: {
+        id: number;
+        data: {
+            bannerUrl: string;
+            name: string;
+            description: string;
+            verified: boolean;
+        };
+    }[] = [];
+
     const database = [
         {
             id: 1,
@@ -47,47 +57,59 @@
     </div>
 </section>
 <main class="flex justify-center items-center flex-col">
-    <section id="trends" class="w-fit mt-16">
-        <div class="flex flex-row justify-center items-center w-fit mb-4">
-            <img
-                src="/trophy-icon.svg"
-                alt="Trophy icon, standing for the trends"
-            />
-            <Text fontSize="text-5xl" fontWeight="font-bold">Trends :</Text>
-        </div>
-        <Grid>
-            {#each database as db}
-                <Card
-                    name={db.data.name}
-                    description={db.data.description}
-                    bannerUrl={db.data.bannerUrl}
-                    id={db.id}
-                    isVerified={db.data.verified}
+    {#if database.length != 0}
+        <section id="trends" class="w-fit mt-16">
+            <div class="flex flex-row justify-center items-center w-fit mb-4">
+                <img
+                    src="/trophy-icon.svg"
+                    alt="Trophy icon, standing for the trends"
                 />
-            {/each}
-        </Grid>
-    </section>
-    <section id="all" class="w-fit mt-20">
-        <div class="flex flex-row justify-center items-center w-fit mb-4">
-            <img
-                src="/puzzle-icon.svg"
-                alt="Puzzle icon, standing for all the extensions"
-                class="mr-8"
-            />
-            <Text fontSize="text-5xl" fontWeight="font-bold"
-                >All Extensions :</Text
-            >
+                <Text fontSize="text-5xl" fontWeight="font-bold">Trends :</Text>
+            </div>
+            <Grid>
+                {#each database as db}
+                    <Card
+                        name={db.data.name}
+                        description={db.data.description}
+                        bannerUrl={db.data.bannerUrl}
+                        id={db.id}
+                        isVerified={db.data.verified}
+                    />
+                {/each}
+            </Grid>
+        </section>
+    {:else}
+        <div class="w-full flex justify-center items-center h-8">
+            <p>Loading data...</p>
         </div>
-        <Grid>
-            {#each database as db}
-                <Card
-                    name={db.data.name}
-                    description={db.data.description}
-                    bannerUrl={db.data.bannerUrl}
-                    id={db.id}
-                    isVerified={db.data.verified}
+    {/if}
+    {#if database.length != 0}
+        <section id="all" class="w-fit mt-20">
+            <div class="flex flex-row justify-center items-center w-fit mb-4">
+                <img
+                    src="/puzzle-icon.svg"
+                    alt="Puzzle icon, standing for all the extensions"
+                    class="mr-8"
                 />
-            {/each}
-        </Grid>
-    </section>
+                <Text fontSize="text-5xl" fontWeight="font-bold"
+                    >All Extensions :</Text
+                >
+            </div>
+            <Grid>
+                {#each database as db}
+                    <Card
+                        name={db.data.name}
+                        description={db.data.description}
+                        bannerUrl={db.data.bannerUrl}
+                        id={db.id}
+                        isVerified={db.data.verified}
+                    />
+                {/each}
+            </Grid>
+        </section>
+    {:else}
+        <div class="w-full flex justify-center items-center h-8">
+            <p>Nothing to show there !</p>
+        </div>
+    {/if}
 </main>
